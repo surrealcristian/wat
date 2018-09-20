cffb1b0dfe88f12a820b8c7208b22d81888a7ab5
Cristian Cabrera <surrealcristian@gmail.com>
2018-09-20 16:28:55 -0300

    WIP

--- wat.c
+++ wat.c
@@ -2,26 +2,20 @@
 #include <math.h>
 #include "SDL.h"
 
-#define WINDOW_WIDTH 480
-#define WINDOW_HEIGHT 480
+#define WINDOW_W 480
+#define WINDOW_H 480
 
 #define ACTOR_N 3
 #define ACTOR_RECT_N 1
 
 
-// ============================================================================
-// ACTOR INPUT COMPONENT
-// ============================================================================
+// NAV_ActorInputComponent
 struct ActorInputComponent {
 };
-// ============================================================================
-// END ACTOR INPUT COMPONENT
-// ============================================================================
+// END NAV_ActorInputComponent
 
 
-// ============================================================================
-// ACTOR GRAPHICS COMPONENT
-// ============================================================================
+// NAV_ActorGraphicsComponent
 struct ActorGraphicsComponent {
     struct Actor *actor;
     float x;
@@ -35,35 +29,28 @@ void actor_graphics_component_set_actor(struct ActorGraphicsComponent *self, str
 void actor_graphics_component_set_position(struct ActorGraphicsComponent *self, float x, float y);
 void actor_graphics_component_update(struct ActorGraphicsComponent *self, SDL_Renderer *renderer);
 
-void actor_graphics_component_set_actor(struct ActorGraphicsComponent *self, struct Actor *actor)
-{
+void actor_graphics_component_set_actor(struct ActorGraphicsComponent *self, struct Actor *actor) {
     self->actor = actor;
 }
 
-void actor_graphics_component_set_position(struct ActorGraphicsComponent *self, float x, float y)
-{
+void actor_graphics_component_set_position(struct ActorGraphicsComponent *self, float x, float y) {
     self->x = x;
     self->y = y;
 
     //TODO: Update SDL_Rects
 }
 
-void actor_graphics_component_update(struct ActorGraphicsComponent *self, SDL_Renderer *renderer)
-{
+void actor_graphics_component_update(struct ActorGraphicsComponent *self, SDL_Renderer *renderer) {
     for (int i = 0; i < ACTOR_RECT_N; i++) {
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 
         SDL_RenderFillRect(renderer, &self->rects[i]);
     }
 }
-// ============================================================================
-// END ACTOR GRAPHICS COMPONENT
-// ============================================================================
+// END NAV_ActorGraphicsComponent
 
 
-// ============================================================================
-// ACTOR GRAPHICS COMPONENT MANAGER
-// ============================================================================
+// NAV_ActorGraphicsComponentManager
 struct ActorGraphicsComponentManager {
     struct ActorGraphicsComponent *actor_graphics_components;
 };
@@ -71,8 +58,7 @@ struct ActorGraphicsComponentManager {
 void actor_graphics_component_manager_init(struct ActorGraphicsComponentManager *self, struct ActorGraphicsComponent *actor_graphics_components);
 void actor_graphics_component_manager_update(struct ActorGraphicsComponentManager *self, SDL_Renderer *renderer);
 
-void actor_graphics_component_manager_init(struct ActorGraphicsComponentManager *self, struct ActorGraphicsComponent *actor_graphics_components)
-{
+void actor_graphics_component_manager_init(struct ActorGraphicsComponentManager *self, struct ActorGraphicsComponent *actor_graphics_components) {
     self->actor_graphics_components = actor_graphics_components;
 
     for (int i = 0; i < ACTOR_N; i++) {
@@ -94,25 +80,21 @@ void actor_graphics_component_manager_init(struct ActorGraphicsComponentManager 
     }
 }
 
-void actor_graphics_component_manager_update(struct ActorGraphicsComponentManager *self, SDL_Renderer *renderer)
-{
+void actor_graphics_component_manager_update(struct ActorGraphicsComponentManager *self, SDL_Renderer *renderer) {
     for (int i = 0; i < ACTOR_N; i++) {
         actor_graphics_component_update(&self->actor_graphics_components[i], renderer);
     }
 }
-// ============================================================================
-// END ACTOR GRAPHICS COMPONENT MANAGER
-// ============================================================================
+// END NAV_ActorGraphicsComponentManager
 
-// ============================================================================
-// ACTOR
-// ============================================================================
+
+// NAV_Actor
 struct Actor {
     struct ActorGraphicsComponent *actor_graphics_component;
     float x;
     float y;
-    int width;
-    int height;
+    int w;
+    int h;
     int velocity;
     int angle;
 };
@@ -122,226 +104,127 @@ float actor_get_x(struct Actor *self);
 void actor_set_x(struct Actor *self, float value);
 float actor_get_y(struct Actor *self);
 void actor_set_y(struct Actor *self, float value);
-int actor_get_width(struct Actor *self);
-void actor_set_width(struct Actor *self, int value);
-int actor_get_height(struct Actor *self);
-void actor_set_height(struct Actor *self, int value);
+int actor_get_w(struct Actor *self);
+void actor_set_w(struct Actor *self, int value);
+int actor_get_h(struct Actor *self);
+void actor_set_h(struct Actor *self, int value);
 int actor_get_velocity(struct Actor *self);
 void actor_set_velocity(struct Actor *self, int value);
 int actor_get_angle(struct Actor *self);
 void actor_set_angle(struct Actor *self, int value);
-int actor_render(struct Actor *self, SDL_Renderer *renderer);
 
 
-void actor_set_actor_graphics_component(struct Actor *self, struct ActorGraphicsComponent *actor_graphics_component)
-{
+void actor_set_actor_graphics_component(struct Actor *self, struct ActorGraphicsComponent *actor_graphics_component) {
     self->actor_graphics_component = actor_graphics_component;
 }
 
-float actor_get_x(struct Actor *self)
-{
+float actor_get_x(struct Actor *self) {
     return self->x;
 }
 
-void actor_set_x(struct Actor *self, float value)
-{
+void actor_set_x(struct Actor *self, float value) {
     if (value < 0) {
         value = 0;
-    } else if (value > WINDOW_WIDTH) {
-        value = WINDOW_WIDTH;
+    } else if (value > WINDOW_W) {
+        value = WINDOW_W;
     }
 
     self->x = value;
 
-    /*
-    SDL_Rect *p;
-
-    for(
-        p = (SDL_Rect*)utarray_front(self->rects);
-        p != NULL;
-        p = (SDL_Rect*)utarray_next(self->rects, p)
-    ) {
-        p->x = floor(self->x - (self->width / 2));
-    }
-    */
+    actor_graphics_component_set_position(self->actor_graphics_component, self->x, self->y);
 }
 
-float actor_get_y(struct Actor *self)
-{
+float actor_get_y(struct Actor *self) {
     return self->y;
 }
 
-void actor_set_y(struct Actor *self, float value)
-{
+void actor_set_y(struct Actor *self, float value) {
     if (value < 0) {
         value = 0;
-    } else if (value > WINDOW_HEIGHT) {
-        value = WINDOW_HEIGHT;
+    } else if (value > WINDOW_H) {
+        value = WINDOW_H;
     }
 
     self->y = value;
 
-    /*
-    SDL_Rect *p;
-
-    for(
-        p = (SDL_Rect*)utarray_front(self->rects);
-        p != NULL;
-        p = (SDL_Rect*)utarray_next(self->rects, p)
-    ) {
-        p->y = floor(self->y - (self->width / 2));
-    }
-    */
+    actor_graphics_component_set_position(self->actor_graphics_component, self->x, self->y);
 }
 
-int actor_get_width(struct Actor *self)
-{
-    return self->width;
+int actor_get_w(struct Actor *self) {
+    return self->w;
 }
 
-void actor_set_width(struct Actor *self, int value)
-{
-    self->width = value;
-
-    /*
-    SDL_Rect *p;
-
-    for(
-        p = (SDL_Rect*)utarray_front(self->rects);
-        p != NULL;
-        p = (SDL_Rect*)utarray_next(self->rects, p)
-    ) {
-        p->w = value;
-    }
-    */
+void actor_set_w(struct Actor *self, int value) {
+    self->w = value;
 }
 
-int actor_get_height(struct Actor *self)
-{
-    return self->height;
+int actor_get_h(struct Actor *self) {
+    return self->h;
 }
 
-void actor_set_height(struct Actor *self, int value)
-{
-    self->height = value;
-
-    /*
-    SDL_Rect *p;
-
-    for(
-        p = (SDL_Rect*)utarray_front(self->rects);
-        p != NULL;
-        p = (SDL_Rect*)utarray_next(self->rects, p)
-    ) {
-        p->h = value;
-    }
-    */
+void actor_set_h(struct Actor *self, int value) {
+    self->h = value;
 }
 
-int actor_get_velocity(struct Actor *self)
-{
+int actor_get_velocity(struct Actor *self) {
     return self->velocity;
 }
 
-void actor_set_velocity(struct Actor *self, int value)
-{
+void actor_set_velocity(struct Actor *self, int value) {
     self->velocity = value;
 }
 
-int actor_get_angle(struct Actor *self)
-{
+int actor_get_angle(struct Actor *self) {
     return self->angle;
 }
 
-void actor_set_angle(struct Actor *self, int value)
-{
+void actor_set_angle(struct Actor *self, int value) {
     self->angle = value;
 }
+// END NAV_Actor
 
-int actor_render(struct Actor *self, SDL_Renderer *renderer)
-{
-    /*
-    SDL_Rect *p;
 
-    for(
-        p = (SDL_Rect*)utarray_front(self->rects);
-        p != NULL;
-        p = (SDL_Rect*)utarray_next(self->rects, p)
-    ) {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-        int tmp = SDL_RenderFillRect(renderer, p);
+// NAV_ActorManager
+struct ActorManager {
+    struct Actor *actors;
+};
 
-        if (tmp != 0) {
-            return -1;
-        }
+void actor_manager_init(struct ActorManager *self, struct Actor *actors, struct ActorGraphicsComponent *actor_graphics_components);
+
+void actor_manager_init(struct ActorManager *self, struct Actor *actors, struct ActorGraphicsComponent *actor_graphics_components) {
+    self->actors = actors;
+
+    for (int i = 0; i < ACTOR_N; i++) {
+        struct Actor *a = &self->actors[i];
+
+        a->actor_graphics_component = &actor_graphics_components[i];
+
+        a->x = 0;
+        a->y = 0;
+        a->w = 20;
+        a->h = 40;
+        a->velocity = 0;
+        a->angle = 0;
     }
-    */
-
-    return 0;
 }
-// ============================================================================
-// END ACTOR
-// ============================================================================
+// END NAV_ActorManager
 
 
 struct ActorGraphicsComponent actor_graphics_component_array[ACTOR_N];
 struct ActorGraphicsComponentManager actor_graphics_component_manager;
+struct Actor actor_array[ACTOR_N];
+struct ActorManager actor_manager;
+struct Game game;
 
 
-// ============================================================================
-// GAME
-// ============================================================================
+// NAV_Game
 struct Game {
 
 };
 
-struct Game *game_new(void);
-void game_free(struct Game *self);
 void game_run(struct Game *game, SDL_Renderer *renderer);
 
-struct Game *game_new(void)
-{
-    struct Game *out = malloc(sizeof(struct Game));
-
-    return out;
-}
-
-void game_free(struct Game *self)
-{
-    free(self);
-}
-
-void game_run(struct Game *self, SDL_Renderer *renderer)
-{
-    /*
-    // actor1
-    struct Actor *actor1 = actor_new();
-    actor_set_width(actor1, 32);
-    actor_set_height(actor1, 32);
-    actor_set_velocity(actor1, 7);
-    actor_set_angle(actor1, 7); 
-    actor_set_x(actor1, 300);
-    actor_set_y(actor1, 300);
-
-    // actor2
-    struct Actor *actor2 = actor_new();
-    actor_set_width(actor2, 32);
-    actor_set_height(actor2, 32);
-    actor_set_velocity(actor2, 7);
-    actor_set_angle(actor2, 7); 
-    actor_set_x(actor2, 100);
-    actor_set_y(actor2, 100);
-
-    // actor3
-    struct Actor *actor3 = actor_new();
-    actor_set_width(actor3, 32);
-    actor_set_height(actor3, 32);
-    actor_set_velocity(actor3, 7);
-    actor_set_angle(actor3, 7); 
-    actor_set_x(actor3, 200);
-    actor_set_y(actor3, 200);
-    */
-
+void game_run(struct Game *self, SDL_Renderer *renderer) {
     int key_left_pressed = 0;
     int key_right_pressed = 0;
     int key_up_pressed = 0;
@@ -513,17 +396,12 @@ void game_run(struct Game *self, SDL_Renderer *renderer)
         loop_tick_ms = (double)((loop_tick_end - loop_tick_start) * 1000) / SDL_GetPerformanceFrequency();
     }
 }
-// ============================================================================
-// END GAME
-// ============================================================================
+// END NAV_Game
 
 
-// ============================================================================
-// MAIN
-// ============================================================================
-int main(void)
-{
-    // [init SDL]
+// NAV_Main
+int main(void) {
+    // init SDL
 
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
         SDL_Log("ERROR: SDL_Init() (%s)", SDL_GetError());
@@ -532,14 +410,14 @@ int main(void)
 
     SDL_Log("INFO: SDL_Init()");
 
-    // [create window]
+    // create window
 
     SDL_Window *window = SDL_CreateWindow(
         "SHMUP",
         SDL_WINDOWPOS_UNDEFINED,
         SDL_WINDOWPOS_UNDEFINED,
-        WINDOW_WIDTH,
-        WINDOW_HEIGHT,
+        WINDOW_W,
+        WINDOW_H,
         0
     );
 
@@ -554,7 +432,7 @@ int main(void)
 
     SDL_Log("INFO: SDL_CreateWindow()");
 
-    // [create renderer]
+    // create renderer
 
     SDL_Renderer *renderer = SDL_CreateRenderer(
         window,
@@ -579,28 +457,25 @@ int main(void)
     SDL_Log("INFO: SDL_CreateRenderer()");
 
     actor_graphics_component_manager_init(&actor_graphics_component_manager, actor_graphics_component_array);
+    actor_manager_init(&actor_manager, actor_array, actor_graphics_component_array);
 
-    // [create game]
-    struct Game *game = game_new();
-    game_run(game, renderer);
+    game_run(&game, renderer);
 
-    // [destroy renderer]
+    // destroy renderer
 
     SDL_DestroyRenderer(renderer);
     SDL_Log("INFO: SDL_DestroyRenderer()");
 
-    // [destroy window]
+    // destroy window
 
     SDL_DestroyWindow(window);
     SDL_Log("INFO: SDL_DestroyWindow()");
 
-    // [quit SDL]
+    // quit SDL
 
     SDL_Quit();
     SDL_Log("INFO: SDL_Quit()");
 
     return 0;
 }
-// ============================================================================
-// END MAIN
-// ============================================================================
+// END NAV_Main

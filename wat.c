4b69bde7f1d169f00417adc00764dc918868a2be
Cristian Cabrera <surrealcristian@gmail.com>
2018-10-05 08:59:57 -0300

    WIP

--- wat.c
+++ wat.c
@@ -34,6 +34,414 @@ float PLAYER_BULLETS_OFFSET_Y[] = { -1.0, -0.50, -0.50, +0.00, +0.00 };
 // END NAV_Config
 
 
+// NAV_Libs
+
+// NAV_TinyMT
+
+// http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
+// http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/VERSIONS/C-LANG/c-lang.html
+// http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/TINYMT/index.html
+
+#ifndef TINYMT32_H
+#define TINYMT32_H
+/**
+ * @file tinymt32.h
+ *
+ * @brief Tiny Mersenne Twister only 127 bit internal state
+ *
+ * @author Mutsuo Saito (Hiroshima University)
+ * @author Makoto Matsumoto (University of Tokyo)
+ *
+ * Copyright (C) 2011 Mutsuo Saito, Makoto Matsumoto,
+ * Hiroshima University and The University of Tokyo.
+ * All rights reserved.
+ *
+ * The 3-clause BSD License is applied to this software, see
+ * LICENSE.txt
+ */
+
+#include <stdint.h>
+#include <inttypes.h>
+
+#define TINYMT32_MEXP 127
+#define TINYMT32_SH0 1
+#define TINYMT32_SH1 10
+#define TINYMT32_SH8 8
+#define TINYMT32_MASK UINT32_C(0x7fffffff)
+#define TINYMT32_MUL (1.0f / 16777216.0f)
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/**
+ * tinymt32 internal state vector and parameters
+ */
+struct TINYMT32_T {
+    uint32_t status[4];
+    uint32_t mat1;
+    uint32_t mat2;
+    uint32_t tmat;
+};
+
+typedef struct TINYMT32_T tinymt32_t;
+
+void tinymt32_init(tinymt32_t * random, uint32_t seed);
+void tinymt32_init_by_array(tinymt32_t * random, uint32_t init_key[],
+			    int key_length);
+
+#if defined(__GNUC__)
+/**
+ * This function always returns 127
+ * @param random not used
+ * @return always 127
+ */
+inline static int tinymt32_get_mexp(
+    tinymt32_t * random  __attribute__((unused))) {
+    return TINYMT32_MEXP;
+}
+#else
+inline static int tinymt32_get_mexp(tinymt32_t * random) {
+    return TINYMT32_MEXP;
+}
+#endif
+
+/**
+ * This function changes internal state of tinymt32.
+ * Users should not call this function directly.
+ * @param random tinymt internal status
+ */
+inline static void tinymt32_next_state(tinymt32_t * random) {
+    uint32_t x;
+    uint32_t y;
+
+    y = random->status[3];
+    x = (random->status[0] & TINYMT32_MASK)
+	^ random->status[1]
+	^ random->status[2];
+    x ^= (x << TINYMT32_SH0);
+    y ^= (y >> TINYMT32_SH0) ^ x;
+    random->status[0] = random->status[1];
+    random->status[1] = random->status[2];
+    random->status[2] = x ^ (y << TINYMT32_SH1);
+    random->status[3] = y;
+    random->status[1] ^= -((int32_t)(y & 1)) & random->mat1;
+    random->status[2] ^= -((int32_t)(y & 1)) & random->mat2;
+}
+
+/**
+ * This function outputs 32-bit unsigned integer from internal state.
+ * Users should not call this function directly.
+ * @param random tinymt internal status
+ * @return 32-bit unsigned pseudorandom number
+ */
+inline static uint32_t tinymt32_temper(tinymt32_t * random) {
+    uint32_t t0, t1;
+    t0 = random->status[3];
+#if defined(LINEARITY_CHECK)
+    t1 = random->status[0]
+	^ (random->status[2] >> TINYMT32_SH8);
+#else
+    t1 = random->status[0]
+	+ (random->status[2] >> TINYMT32_SH8);
+#endif
+    t0 ^= t1;
+    t0 ^= -((int32_t)(t1 & 1)) & random->tmat;
+    return t0;
+}
+
+/**
+ * This function outputs floating point number from internal state.
+ * Users should not call this function directly.
+ * @param random tinymt internal status
+ * @return floating point number r (1.0 <= r < 2.0)
+ */
+inline static float tinymt32_temper_conv(tinymt32_t * random) {
+    uint32_t t0, t1;
+    union {
+	uint32_t u;
+	float f;
+    } conv;
+
+    t0 = random->status[3];
+#if defined(LINEARITY_CHECK)
+    t1 = random->status[0]
+	^ (random->status[2] >> TINYMT32_SH8);
+#else
+    t1 = random->status[0]
+	+ (random->status[2] >> TINYMT32_SH8);
+#endif
+    t0 ^= t1;
+    conv.u = ((t0 ^ (-((int32_t)(t1 & 1)) & random->tmat)) >> 9)
+	      | UINT32_C(0x3f800000);
+    return conv.f;
+}
+
+/**
+ * This function outputs floating point number from internal state.
+ * Users should not call this function directly.
+ * @param random tinymt internal status
+ * @return floating point number r (1.0 < r < 2.0)
+ */
+inline static float tinymt32_temper_conv_open(tinymt32_t * random) {
+    uint32_t t0, t1;
+    union {
+	uint32_t u;
+	float f;
+    } conv;
+
+    t0 = random->status[3];
+#if defined(LINEARITY_CHECK)
+    t1 = random->status[0]
+	^ (random->status[2] >> TINYMT32_SH8);
+#else
+    t1 = random->status[0]
+	+ (random->status[2] >> TINYMT32_SH8);
+#endif
+    t0 ^= t1;
+    conv.u = ((t0 ^ (-((int32_t)(t1 & 1)) & random->tmat)) >> 9)
+	      | UINT32_C(0x3f800001);
+    return conv.f;
+}
+
+/**
+ * This function outputs 32-bit unsigned integer from internal state.
+ * @param random tinymt internal status
+ * @return 32-bit unsigned integer r (0 <= r < 2^32)
+ */
+inline static uint32_t tinymt32_generate_uint32(tinymt32_t * random) {
+    tinymt32_next_state(random);
+    return tinymt32_temper(random);
+}
+
+/**
+ * This function outputs floating point number from internal state.
+ * This function is implemented using multiplying by (1 / 2^24).
+ * floating point multiplication is faster than using union trick in
+ * my Intel CPU.
+ * @param random tinymt internal status
+ * @return floating point number r (0.0 <= r < 1.0)
+ */
+inline static float tinymt32_generate_float(tinymt32_t * random) {
+    tinymt32_next_state(random);
+    return (tinymt32_temper(random) >> 8) * TINYMT32_MUL;
+}
+
+/**
+ * This function outputs floating point number from internal state.
+ * This function is implemented using union trick.
+ * @param random tinymt internal status
+ * @return floating point number r (1.0 <= r < 2.0)
+ */
+inline static float tinymt32_generate_float12(tinymt32_t * random) {
+    tinymt32_next_state(random);
+    return tinymt32_temper_conv(random);
+}
+
+/**
+ * This function outputs floating point number from internal state.
+ * This function is implemented using union trick.
+ * @param random tinymt internal status
+ * @return floating point number r (0.0 <= r < 1.0)
+ */
+inline static float tinymt32_generate_float01(tinymt32_t * random) {
+    tinymt32_next_state(random);
+    return tinymt32_temper_conv(random) - 1.0f;
+}
+
+/**
+ * This function outputs floating point number from internal state.
+ * This function may return 1.0 and never returns 0.0.
+ * @param random tinymt internal status
+ * @return floating point number r (0.0 < r <= 1.0)
+ */
+inline static float tinymt32_generate_floatOC(tinymt32_t * random) {
+    tinymt32_next_state(random);
+    return 1.0f - tinymt32_generate_float(random);
+}
+
+/**
+ * This function outputs floating point number from internal state.
+ * This function returns neither 0.0 nor 1.0.
+ * @param random tinymt internal status
+ * @return floating point number r (0.0 < r < 1.0)
+ */
+inline static float tinymt32_generate_floatOO(tinymt32_t * random) {
+    tinymt32_next_state(random);
+    return tinymt32_temper_conv_open(random) - 1.0f;
+}
+
+/**
+ * This function outputs double precision floating point number from
+ * internal state. The returned value has 32-bit precision.
+ * In other words, this function makes one double precision floating point
+ * number from one 32-bit unsigned integer.
+ * @param random tinymt internal status
+ * @return floating point number r (0.0 < r <= 1.0)
+ */
+inline static double tinymt32_generate_32double(tinymt32_t * random) {
+    tinymt32_next_state(random);
+    return tinymt32_temper(random) * (1.0 / 4294967296.0);
+}
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
+
+
+/**
+ * @file tinymt32.c
+ *
+ * @brief Tiny Mersenne Twister only 127 bit internal state
+ *
+ * @author Mutsuo Saito (Hiroshima University)
+ * @author Makoto Matsumoto (The University of Tokyo)
+ *
+ * Copyright (C) 2011 Mutsuo Saito, Makoto Matsumoto,
+ * Hiroshima University and The University of Tokyo.
+ * All rights reserved.
+ *
+ * The 3-clause BSD License is applied to this software, see
+ * LICENSE.txt
+ */
+//#include "tinymt32.h"
+#define MIN_LOOP 8
+#define PRE_LOOP 8
+
+/**
+ * This function represents a function used in the initialization
+ * by init_by_array
+ * @param x 32-bit integer
+ * @return 32-bit integer
+ */
+static uint32_t ini_func1(uint32_t x) {
+    return (x ^ (x >> 27)) * UINT32_C(1664525);
+}
+
+/**
+ * This function represents a function used in the initialization
+ * by init_by_array
+ * @param x 32-bit integer
+ * @return 32-bit integer
+ */
+static uint32_t ini_func2(uint32_t x) {
+    return (x ^ (x >> 27)) * UINT32_C(1566083941);
+}
+
+/**
+ * This function certificate the period of 2^127-1.
+ * @param random tinymt state vector.
+ */
+static void period_certification(tinymt32_t * random) {
+    if ((random->status[0] & TINYMT32_MASK) == 0 &&
+	random->status[1] == 0 &&
+	random->status[2] == 0 &&
+	random->status[3] == 0) {
+	random->status[0] = 'T';
+	random->status[1] = 'I';
+	random->status[2] = 'N';
+	random->status[3] = 'Y';
+    }
+}
+
+/**
+ * This function initializes the internal state array with a 32-bit
+ * unsigned integer seed.
+ * @param random tinymt state vector.
+ * @param seed a 32-bit unsigned integer used as a seed.
+ */
+void tinymt32_init(tinymt32_t * random, uint32_t seed) {
+    random->status[0] = seed;
+    random->status[1] = random->mat1;
+    random->status[2] = random->mat2;
+    random->status[3] = random->tmat;
+    for (int i = 1; i < MIN_LOOP; i++) {
+	random->status[i & 3] ^= i + UINT32_C(1812433253)
+	    * (random->status[(i - 1) & 3]
+	       ^ (random->status[(i - 1) & 3] >> 30));
+    }
+    period_certification(random);
+    for (int i = 0; i < PRE_LOOP; i++) {
+	tinymt32_next_state(random);
+    }
+}
+
+/**
+ * This function initializes the internal state array,
+ * with an array of 32-bit unsigned integers used as seeds
+ * @param random tinymt state vector.
+ * @param init_key the array of 32-bit integers, used as a seed.
+ * @param key_length the length of init_key.
+ */
+void tinymt32_init_by_array(tinymt32_t * random, uint32_t init_key[],
+			    int key_length) {
+    const int lag = 1;
+    const int mid = 1;
+    const int size = 4;
+    int i, j;
+    int count;
+    uint32_t r;
+    uint32_t * st = &random->status[0];
+
+    st[0] = 0;
+    st[1] = random->mat1;
+    st[2] = random->mat2;
+    st[3] = random->tmat;
+    if (key_length + 1 > MIN_LOOP) {
+	count = key_length + 1;
+    } else {
+	count = MIN_LOOP;
+    }
+    r = ini_func1(st[0] ^ st[mid % size]
+		  ^ st[(size - 1) % size]);
+    st[mid % size] += r;
+    r += key_length;
+    st[(mid + lag) % size] += r;
+    st[0] = r;
+    count--;
+    for (i = 1, j = 0; (j < count) && (j < key_length); j++) {
+	r = ini_func1(st[i % size]
+		      ^ st[(i + mid) % size]
+		      ^ st[(i + size - 1) % size]);
+	st[(i + mid) % size] += r;
+	r += init_key[j] + i;
+	st[(i + mid + lag) % size] += r;
+	st[i % size] = r;
+	i = (i + 1) % size;
+    }
+    for (; j < count; j++) {
+	r = ini_func1(st[i % size]
+		      ^ st[(i + mid) % size]
+		      ^ st[(i + size - 1) % size]);
+	st[(i + mid) % size] += r;
+	r += i;
+	st[(i + mid + lag) % size] += r;
+	st[i % size] = r;
+	i = (i + 1) % size;
+    }
+    for (j = 0; j < size; j++) {
+	r = ini_func2(st[i % size]
+		      + st[(i + mid) % size]
+		      + st[(i + size - 1) % size]);
+	st[(i + mid) % size] ^= r;
+	r -= i;
+	st[(i + mid + lag) % size] ^= r;
+	st[i % size] = r;
+	i = (i + 1) % size;
+    }
+    period_certification(random);
+    for (i = 0; i < PRE_LOOP; i++) {
+	tinymt32_next_state(random);
+    }
+}
+
+// END NAV_TinyMT
+
+// END NAV_Libs
+
+
 // NAV_Declarations
 
 // NAV_Keys
@@ -73,9 +481,7 @@ struct Player {
 };
 
 void player_init(struct Player *self, struct BulletManager *bullet_manager, float x, float y, int w, int h, int v);
-float player_get_x(struct Player *self);
 void player_set_x(struct Player *self, float value);
-float player_get_y(struct Player *self);
 void player_set_y(struct Player *self, float value);
 void player_on_button_a_keydown(struct Player *self);
 void player_on_button_a_keyup(struct Player *self);
@@ -99,11 +505,7 @@ struct Enemy {
 };
 
 void enemy_init(struct Enemy *self, float x, float y, int w, int h, int v);
-float enemy_get_x(struct Enemy *self);
 void enemy_set_x(struct Enemy *self, float value);
-float enemy_get_y(struct Enemy *self);
-void enemy_set_vx(struct Enemy *self, float value);
-void enemy_set_vy(struct Enemy *self, float value);
 void enemy_set_y(struct Enemy *self, float value);
 void enemy_update(struct Enemy *self);
 void enemy_render(struct Enemy *self, SDL_Renderer *renderer);
@@ -143,9 +545,6 @@ float bullet_get_x(struct Bullet *self);
 void bullet_set_x(struct Bullet *self, float value);
 float bullet_get_y(struct Bullet *self);
 void bullet_set_y(struct Bullet *self, float value);
-int bullet_get_v(struct Bullet *self);
-void bullet_set_vx(struct Bullet *self, int value);
-void bullet_set_vy(struct Bullet *self, int value);
 void bullet_update(struct Bullet *self);
 void bullet_render(struct Bullet *self, SDL_Renderer *renderer);
 // ENV NAV_Bullet
@@ -166,6 +565,11 @@ void bullet_manager_render(struct BulletManager *self, SDL_Renderer *renderer);
 
 // NAV_MiscFunctions
 double performance_counters_to_ms(Uint64 start, Uint64 end);
+
+#define randN(N) ((int)(nextRand()>>5)%(N))
+#define randNS(N) (((int)(nextRand()>>5))%(N<<1)-N)
+#define randNS2(N) ((((int)(nextRand()>>5))%(N)-(N>>1)) + (((int)(nextRand()>>5))%(N)-(N>>1)))
+#define absN(a) ((a) < 0 ? - (a) : (a))
 // END NAV_MiscFunctions
 
 
@@ -269,10 +673,6 @@ void player_init(struct Player *self, struct BulletManager *bullet_manager, floa
     self->fire_spacing = 128;
 }
 
-float player_get_x(struct Player *self) {
-    return self->x;
-}
-
 void player_set_x(struct Player *self, float value) {
     float x_min = 0 + self->w / 2;
     float x_max = WINDOW_W - self->w / 2;
@@ -286,10 +686,6 @@ void player_set_x(struct Player *self, float value) {
     self->x = value;
 
     self->rect.x = floor(self->x - (self->w / 2));
-}
-
-float player_get_y(struct Player *self) {
-    return self->y;
 }
 
 void player_set_y(struct Player *self, float value) {
@@ -329,8 +725,9 @@ void player_fire(struct Player *self) {
 
         bullet_set_x(bullet, self->x + self->w * PLAYER_BULLETS_OFFSET_X[i]);
         bullet_set_y(bullet, self->y + self->h * PLAYER_BULLETS_OFFSET_Y[i]);
-        bullet_set_vx(bullet, PLAYER_BULLETS_VX);
-        bullet_set_vy(bullet, PLAYER_BULLETS_VY);
+
+        bullet->vx = PLAYER_BULLETS_VX;
+        bullet->vy = PLAYER_BULLETS_VY;
     }
 }
 
@@ -389,18 +786,13 @@ void enemy_init(struct Enemy *self, float x, float y, int w, int h, int v) {
     self->alive = 0;
 }
 
-float enemy_get_x(struct Enemy *self) {
-    return self->x;
-}
-
 void enemy_set_x(struct Enemy *self, float value) {
-    float x_min = 0 + self->w / 2;
-    float x_max = WINDOW_W - self->w / 2;
+    float x_min = 0 - self->w + 1;
+    float x_max = WINDOW_W + self->w + 1;
 
-    if (value < x_min) {
-        value = x_min;
-    } else if (value > x_max) {
-        value = x_max;
+    if (value < x_min || value > x_max) {
+        self->alive = 0;
+        return;
     }
 
     self->x = value;
@@ -408,31 +800,18 @@ void enemy_set_x(struct Enemy *self, float value) {
     self->rect.x = floor(self->x - (self->w / 2));
 }
 
-float enemy_get_y(struct Enemy *self) {
-    return self->y;
-}
-
 void enemy_set_y(struct Enemy *self, float value) {
-    float y_min = 0 + self->h / 2;
-    float y_max = WINDOW_H - self->h / 2;
+    float y_min = 0 - self->h - 1;
+    float y_max = WINDOW_H + self->h + 1;
 
-    if (value < y_min) {
-        value = y_min;
-    } else if (value > y_max) {
-        value = y_max;
+    if (value < y_min || value > y_max) {
+        self->alive = 0;
+        return;
     }
 
     self->y = value;
 
     self->rect.y = floor(self->y - (self->h / 2));
-}
-
-void enemy_set_vx(struct Enemy *self, float value) {
-    self->vx = value;
-}
-
-void enemy_set_vy(struct Enemy *self, float value) {
-    self->vy = value;
 }
 
 void enemy_update(struct Enemy *self) {
@@ -479,12 +858,17 @@ struct Enemy *enemy_manager_get_free(struct EnemyManager *self) {
 void enemy_manager_spawn(struct EnemyManager *self) {
     struct Enemy *enemy = enemy_manager_get_free(self);
 
-    enemy->alive = 1; //TODO: enemy_set_alive()
+    if (enemy == NULL) {
+        return;
+    }
+
+    enemy->alive = 1;
 
     enemy_set_x(enemy, 50);
-    enemy_set_y(enemy, 0);
-    enemy_set_vx(enemy, ENEMY_VX);
-    enemy_set_vy(enemy, ENEMY_VY);
+    enemy_set_y(enemy, 0 - enemy->h);
+
+    enemy->vx = ENEMY_VX;
+    enemy->vy = ENEMY_VY;
 }
 
 void enemy_manager_update(struct EnemyManager *self) {
@@ -537,8 +921,8 @@ float bullet_get_x(struct Bullet *self) {
 }
 
 void bullet_set_x(struct Bullet *self, float value) {
-    float x_min = 0;
-    float x_max = WINDOW_W;
+    float x_min = 0 - self->w - 1;
+    float x_max = WINDOW_W + self->w + 1;
 
     if (value < x_min || value > x_max) {
         self->alive = 0;
@@ -555,8 +939,8 @@ float bullet_get_y(struct Bullet *self) {
 }
 
 void bullet_set_y(struct Bullet *self, float value) {
-    float y_min = 0;
-    float y_max = WINDOW_H;
+    float y_min = 0 - self->h - 1;
+    float y_max = WINDOW_H + self->h + 1;
 
     if (value < y_min || value > y_max) {
         self->alive = 0;
@@ -566,18 +950,6 @@ void bullet_set_y(struct Bullet *self, float value) {
     self->y = value;
 
     self->rect.y = floor(self->y - (self->h / 2));
-}
-
-int bullet_get_v(struct Bullet *self) {
-    return self->v;
-}
-
-void bullet_set_vx(struct Bullet *self, int value) {
-    self->vx = value;
-}
-
-void bullet_set_vy(struct Bullet *self, int value) {
-    self->vy = value;
 }
 
 void bullet_update(struct Bullet *self) {
